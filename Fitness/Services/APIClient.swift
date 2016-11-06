890c47c5b9b01d9dea9cc4afcc099ff3ab1a76ae
Josep Bordes Jové <josepbordesjove@gmail.com>
2016-11-06 11:11:54 +0100

    Write the needed protocol for the APIClient

--- Fitness/Services/APIClient.swift
+++ Fitness/Services/APIClient.swift
@@ -1,1 +1,28 @@
 import Foundation
+
+public let JBJNetworkingErrorDomain = "com.josepbordes.apiawakens"
+public let MissingHTTPResponseError: Int = 10
+public let UnexpectedResponseError: Int = 20
+
+typealias JSON = [String : AnyObject]
+typealias JSONTaskCompletion = (JSON?, HTTPURLResponse?, NSError?) -> Void
+typealias JSONTask = URLSessionDataTask
+
+enum APIResult<T> {
+  case Success(T)
+  case Failure(Error)
+}
+
+protocol JSONDecodable {
+  init?(JSON: [String : AnyObject])
+}
+
+protocol Endpoint {
+  var baseURL: String { get }
+  var path: String { get }
+}
+
+protocol APIClient {
+  var configuration: URLSessionConfiguration { get }
+  var session: URLSession { get }
+}
